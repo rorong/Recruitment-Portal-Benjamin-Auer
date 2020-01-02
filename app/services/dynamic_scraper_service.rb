9207bdefdbf913b71912b7a5ba496d9e520576a6
rorong <rordev@ongraph.com>
2020-01-02 16:38:25 +0530

    test revert

--- app/services/dynamic_scraper_service.rb
+++ app/services/dynamic_scraper_service.rb
@@ -16,7 +16,6 @@ class DynamicScraperService
     # if (chrome_bin = ENV.fetch('/app/.apt/usr/bin/google-chrome', nil))
     #   opts.merge!( options: {binary: chrome_bin})
     # end
-
     Headless.ly do
       begin
         jobs = Array.new
