f3e42fc5a84bed9e513e3da3bce5afc1a9668204
rorong <rordev@ongraph.com>
2020-01-08 15:31:30 +0530

    sending mail in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
                                          :address=> "smtp.gmail.com",
                                          :port=> 587,
-                                         :user_name => ENV['gmail_username'],
-                                         :password => ENV['gmail_password'],
+                                         :user_name => 'Jobsearchadvanced@gmail.com',
+                                         :password => 'Testformails',
                                          :authentication => "plain",
                                          :enable_starttls_auto => true
                                       }
