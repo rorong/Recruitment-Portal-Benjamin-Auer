80ab927740956cd6dd5175a953f4a1435cd9625c
ankur <ankur.jain@ongraph.com>
2020-01-24 16:41:18 +0530

    env var foe gmail creds

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,11 +78,12 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+
   config.action_mailer.smtp_settings = {
                                          :address=> "smtp.gmail.com",
                                          :port=> 587,
-                                         :user_name => 'Jobsearchadvanced@gmail.com',
-                                         :password => 'Testformails',
+                                         :user_name => ENV['gmail_username'],
+                                         :password => ENV['gmail_password'],
                                          :authentication => "plain",
                                          :enable_starttls_auto => true
                                       }
