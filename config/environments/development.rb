e4b454ccefeea87c28a32b40b9381710f53c1a0d
ankur <ankur.jain@ongraph.com>
2020-01-27 13:15:18 +0530

    running heroku in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,24 +55,16 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  if true
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  end
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger    = ActiveSupport::TaggedLogging.new(logger)
 
-
-  #sending email
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :address              => "smtp.gmail.com",
-        :port                 => 587,
-        :user_name            => "mohdsaimansari54786@gmail.com",
-        :password             => "ziijpblrfsgjfjgw",
-        :authentication       => "plain",
-        :enable_starttls_auto => true
-  }
-
-  # Specify what domain to use for mailer URLs
-  config.action_mailer.default_url_options = {host: "localhost:3000"}
+                                         :address=> "smtp.gmail.com",
+                                         :port=> 587,
+                                         :user_name => ENV['gmail_username'],
+                                         :password => ENV['gmail_password'],
+                                         :authentication => "plain",
+                                         :enable_starttls_auto => true
+                                      }
 end
