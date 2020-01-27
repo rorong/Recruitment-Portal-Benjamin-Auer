2e790c7fc85219008d2e4c51c07aef219d9302f5
ankur <ankur.jain@ongraph.com>
2020-01-27 13:21:52 +0530

    commenting code to check

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Allow network requests from anywhere
-  config.web_console.whiny_requests = false
+  # config.web_console.whiny_requests = false
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
